0bed3bc9e5a6cce4cc37ecca2a993e9618c0e130
Eric Chen <ericchen201896@gmail.com>
2021-06-23 19:28:10 -0700

    added in answers to 6.

---
+++ 1329_sort_the_matrix_diagonally.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} mat
+ * @return {number[][]}
+ */
+ var diagonalSort = function(mat) {
+    let answer = [...Array(mat.length)].map((element)=> { Array(matrix[0].length)
+        
+    })
+};
+let mat = [[11,25,66,1,69,7],[23,55,17,45,15,52],[75,31,36,44,58,8],[22,27,33,25,68,4],[84,28,14,11,5,50]];
+let answer = [...Array(mat.length)].map((element)=> Array(mat[0].length));
+let answer2 = Array(mat.length).map((element)=> Array(mat[0].length));
+
+console.log(answer);
+console.log(answer2);
